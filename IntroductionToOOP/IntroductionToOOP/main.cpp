88eb410bc0227f2557a07314a3c4b2140973b9b2
Clayman <Clayman@DESKTOP-DM8E77U>
2023-08-24 15:49:34 +0300

    ReadMe

---
+++ IntroductionToOOP/IntroductionToOOP/main.cpp
@@ -0,0 +1,72 @@
+﻿#include<iostream>
+using namespace std;
+
+//Описание структуры
+class Point
+{
+	double x;
+	double y;
+public:
+	double get_x()const
+	{
+		return x;
+	}
+	double get_y()const
+	{
+		return y;
+	}
+	void set_x(double x)
+	{
+		this->x = x;
+	}
+	void set_y(double y)
+	{
+		this->y = y;
+	}
+};
+
+//#define STRUCT_POINT
+
+void main()
+{
+	setlocale(LC_ALL, "");
+
+#ifdef STRUCT_POINT
+	int a;		//Объявление переменной 'a' типа 'int'
+	Point A;	//Объявление переменной 'A' типа 'Point'	
+				//Создание объекта 'A' структуры 'Point'
+				//Создание экземпляра 'A' структуры 'Point'
+	A.x = 2;
+	A.y = 3;
+	cout << A.x << "\t" << A.y << endl;
+
+	Point* pA = &A;
+	cout << pA->x << "\t" << pA->y << endl;
+#endif // STRICT_POINT
+
+	Point A;
+	A.set_x(2);
+	A.set_y(3);
+	cout << A.get_x() << "\t" << A.get_y() << endl;
+}
+
+/*
+-------------------------------------------------------------
+.  - Оператор прямого доступа (Point operator)
+-> - Оператор косвенного доступа (Arrow operator)
+-------------------------------------------------------------
+*/
+
+/*
+-------------------------------------------------------------
+1. Encapsulation
+	private:	закрытые поля, доступны только внутри класса
+	public:		открытые поля, доступны из любого места программы
+	protected:
+
+	get-методы (взять, получить)
+	set-методы (задать, установить)
+2. Inheritance;
+3. Polymorphism;
+-------------------------------------------------------------
+*/
---
+++ ReadMe.txt
@@ -0,0 +1,7 @@
+https://github.com/okovtun/PD_321.git
+https://www.youtube.com/watch?v=DJgLqTRrUiE&list=PLeqyOOqxeiINqPSzLEh9Oq9eIjtKj_rHv
+
+TODO:
+0. Выучить теорию!!!!!!!!!!!!!!!!!!!!!!!!!
+1. Написать метод ??? distance(???), который возвращает расстояние до указанной точки;
+2. Написать функцию ??? distance(???), которая возвращает расстояние между двумя точками;
